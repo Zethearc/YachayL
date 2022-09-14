9bcbdb159cdbaec072f0ff1b4728eff671df59f0
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-09-14 16:27:10 -0500

    Prefix Finished

--- YachayLP/parser.py
+++ YachayLP/parser.py
@@ -1,14 +1,29 @@
 from enum import IntEnum
-from typing import Callable, Dict, List, Optional
-
-from YachayLP.ast import Expression, ExpressionStatement, Identifier, Integer,LetStatement, Program, Statement, ReturnStatement
+from typing import (
+    Callable,
+    Dict,
+    List,
+    Optional,
+)
+from YachayLP.ast import (Expression,
+                        ExpressionStatement,
+                        Identifier,
+                        Integer,
+                        LetStatement,
+                        Prefix,
+                        Program,
+                        ReturnStatement,
+                        Statement,
+                        )
 from YachayLP.lexer import Lexer
 from YachayLP.token import Token, TokenType
+
 
 PrefixParseFn = Callable[[], Optional[Expression]]
 InfixParseFn = Callable[[Expression], Optional[Expression]]
 PrefixParseFns = Dict[TokenType, PrefixParseFn]
 InfixParseFns = Dict[TokenType, InfixParseFn]
+
 
 class Precedence(IntEnum):
     LOWEST = 1
@@ -19,36 +34,38 @@ class Precedence(IntEnum):
     PREFIX = 6
     CALL = 7
 
+
 class Parser:
-    
+
     def __init__(self, lexer: Lexer) -> None:
         self._lexer = lexer
         self._current_token: Optional[Token] = None
         self._peek_token: Optional[Token] = None
-        self._erorrs: List[str] = []
-        
+        self._errors: List[str] = []
+
         self._prefix_parse_fns: PrefixParseFns = self._register_prefix_fns()
         self._infix_parse_fns: InfixParseFns = self._register_infix_fns()
+
         self._advance_tokens()
         self._advance_tokens()
-    
+
     @property
     def errors(self) -> List[str]:
-        return self._erorrs
+        return self._errors
 
     def parse_program(self) -> Program:
-        program: Program = Program(statements = [])
-        
+        program: Program = Program(statements=[])
+
         assert self._current_token is not None
         while self._current_token.token_type != TokenType.EOF:
             statement = self._parse_statement()
             if statement is not None:
                 program.statements.append(statement)
-                
+
             self._advance_tokens()
-        
+
         return program
-    
+
     def _advance_tokens(self) -> None:
         self._current_token = self._peek_token
         self._peek_token = self._lexer.next_token()
@@ -65,40 +82,43 @@ class Parser:
 
     def _expected_token_error(self, token_type: TokenType) -> None:
         assert self._peek_token is not None
-        error = f'Se esperaba que el siguiente token fuera {token_type}' + \
-            f' pero se obtuvo {self._peek_token.token_type}'
-            
-        self.errors.append(error)
+        error = f'Se esperaba que el siguiente tokne fuera {token_type} ' + \
+            f'pero se obtuvo {self._peek_token.token_type}'
+
+        self._errors.append(error)
 
     def _parse_expression(self, precedence: Precedence) -> Optional[Expression]:
         assert self._current_token is not None
         try:
             prefix_parse_fn = self._prefix_parse_fns[self._current_token.token_type]
         except KeyError:
+            message = f'No se encontro ninguna funcion para parsear {self._current_token.literal}'
+            self._errors.append(message)
+
             return None
-        
+
         left_expression = prefix_parse_fn()
-        
+
         return left_expression
 
-    def _parse_expression_statements(self) -> Optional[ExpressionStatement]:
+    def _parse_expression_statement(self) -> Optional[ExpressionStatement]:
         assert self._current_token is not None
         expression_statement = ExpressionStatement(token=self._current_token)
-        
+
         expression_statement.expression = self._parse_expression(Precedence.LOWEST)
-        
+
         assert self._peek_token is not None
         if self._peek_token.token_type == TokenType.SEMICOLON:
             self._advance_tokens()
-            
+
         return expression_statement
-    
+
     def _parse_identifier(self) -> Identifier:
         assert self._current_token is not None
-        
+
         return Identifier(token=self._current_token,
                           value=self._current_token.literal)
-        
+
     def _parse_integer(self) -> Optional[Integer]:
         assert self._current_token is not None
         integer = Integer(token=self._current_token)
@@ -121,7 +141,7 @@ class Parser:
         if not self._expected_token(TokenType.IDENT):
             return None
 
-        let_statement.name = self._parse_identifier()
+        let_statement.name = self._parse_identifier() 
 
         if not self._expected_token(TokenType.ASSIGN):
             return None
@@ -131,7 +151,18 @@ class Parser:
             self._advance_tokens()
 
         return let_statement
-    
+
+    def _parse_prefix_expression(self) -> Prefix:
+        assert self._current_token is not None
+        prefix_expression = Prefix(token=self._current_token,
+                                   operator=self._current_token.literal)
+
+        self._advance_tokens()
+
+        prefix_expression.right = self._parse_expression(Precedence.PREFIX)
+
+        return prefix_expression
+
     def _parse_return_statement(self) -> Optional[ReturnStatement]:
         assert self._current_token is not None
         return_statement = ReturnStatement(token=self._current_token)
@@ -151,13 +182,15 @@ class Parser:
         elif self._current_token.token_type == TokenType.RETURN:
             return self._parse_return_statement()
         else:
-            return self._parse_expression_statements()
-        
+            return self._parse_expression_statement()
+
     def _register_infix_fns(self) -> InfixParseFns:
         return {}
-    
+
     def _register_prefix_fns(self) -> PrefixParseFns:
         return {
             TokenType.IDENT: self._parse_identifier,
             TokenType.INT: self._parse_integer,
+            TokenType.MINUS: self._parse_prefix_expression,
+            TokenType.NOT: self._parse_prefix_expression,
         }
