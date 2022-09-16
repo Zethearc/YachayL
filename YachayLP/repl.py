2f88dd861cdaf6b4d5f25e96b5fb2a00ff1ee85a
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-09-15 19:38:50 -0500

    Segundo REPL

--- YachayLP/repl.py
+++ YachayLP/repl.py
@@ -1,11 +1,23 @@
+from typing import List
 from YachayLP.lexer import Lexer
 from YachayLP.token import Token, TokenType
+from YachayLP.parser import Parser
 
 EOF_TOKEN: Token = Token(TokenType.EOF, '')
+
+def _print_parse_errors(errors: List[str]):
+    for error in errors:
+        print(error)
 
 def start_repl() -> None:
     while (source := input('>> ')) != 'salir()':
         lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+        
+        program: Program = parser.parse_program()
 
-        while (token := lexer.next_token()) != EOF_TOKEN:
-            print(token)
+        if len(parser.errors) > 0:
+            _print_parse_errors(parser.errors)
+            continue
+        
+        print(program)
